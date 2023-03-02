e3004f43672a96230fce11137a672181e0dfcd8a
hbashar434 <h.bashar434@gmail.com>
2023-03-02 22:57:06 +0600

    up to features

--- js/index.js
+++ js/index.js
@@ -10,24 +10,31 @@ const displayAiData = (data) => {
   data.forEach((singleCard) => {
     const { name, image, features, published_in } = singleCard;
     cardContainer.innerHTML += `
-        <div class="aiCard">
-            <div class="card w-full h-full bg-base-100 shadow-xl">
+        <div class="aiCard rounded">
+         <div class="card w-full h-full bg-base-100 shadow-xl">
             <figure><img src="${image}"/>
             </figure>
             <div class="card-body">
-                <h2 class="card-title">
-                Features
-                <div>
-
+                <h2 class="card-title">Features</h2>
+                <div class="ml-5">
+                    <ol class="list-decimal">
+                    <li>${features[0]}</li>
+                    <li>${features[1]}</li>
+                    <li>${features[2]}</li>
+                    <ol>
                 </div>
-                </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                <div class="badge badge-outline">Fashion</div> 
-                <div class="badge badge-outline">Products</div>
+                <hr class="my-4">
+                <div class="flex justify-between items-center">
+                    <div>
+                    <p class="font-bold mb-1">${name}</p>
+                    <p><i class="fa-sharp fa-regular fa-calendar"></i>  ${published_in}</p>
+                    </div>
+                    <div>
+                    <i class="fa-solid fa-arrow-right text-red-700"></i>
+                    </div>
                 </div>
             </div>
-            </div>
+         </div>
         </div>
     `;
   });
